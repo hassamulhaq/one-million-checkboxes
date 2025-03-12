e0732d3b56dfef1160347bf5041d304b0e7d62b2
hassamulhaq <developer.hassam@gmail.com>
2025-03-13 04:31:29 +0500

    Rerender recycle-scroller comp without touching comp area - using ref having current timestamp i rerender the comp

--- resources/js/components/Home/Index.vue
+++ resources/js/components/Home/Index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted } from 'vue';
+import {onMounted, ref} from 'vue';
 import Header from '../Header.vue';
 import { RecycleScroller } from 'vue-virtual-scroller';
 
@@ -10,6 +10,15 @@ const items = [...Array(1_000_000).keys()].map((i) => {
     }
 });
 
+const gridItems = ref(32);
+const itemSize = ref(1216.02 / gridItems.value);
+const recycleKey = ref(Date.now());
+
+const changeState = (id, isChecked) => {
+    items[id - 1].checked = isChecked;
+    recycleKey.value = Date.now();
+}
+
 onMounted(() => {
     console.log({ items });
 });
@@ -17,15 +26,19 @@ onMounted(() => {
 
 <template>
     <Header/>
-    <div class="bg-orange-300 p-2 h-10/12">
+    <button type="button" class="bg-amber-600 hover:bg-amber-700 p-0.5 w-[90px]" @click="changeState(1, true)">click 1st checked</button>
+    <div class="bg-orange-300 p-2 shadow">
         <RecycleScroller
-            class="scroller"
+            class="scroller w-full flex-grow overflow-y-auto overflow-x-auto min-h-10/12 max-h-[680px]"
             :items="items"
-            :item-size="30"
+            :item-size="itemSize"
             key-field="id"
-            v-slot="{ item }">
+            v-slot="{ item }"
+            :grid-items="gridItems"
+            :key="recycleKey"
+        >
             <div class="single-checkbox-wrapper">
-                {{ item }}
+               <input type="checkbox" :checked="item.checked" :data-id="item.id" class="size-6">
             </div>
         </RecycleScroller>
     </div>
