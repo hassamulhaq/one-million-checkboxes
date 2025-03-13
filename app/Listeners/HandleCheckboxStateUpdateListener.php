ae984dbe589197855934423e6b3a55a721cc66f2
hassamulhaq <developer.hassam@gmail.com>
2025-03-13 05:22:31 +0500

    bi-directional websocket communication using reverb MessageReceived event

---
+++ app/Listeners/HandleCheckboxStateUpdateListener.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Support\Facades\Log;
+use Laravel\Reverb\Events\MessageReceived;
+
+class HandleCheckboxStateUpdateListener
+{
+    public function __construct()
+    {
+    }
+
+    public function handle(MessageReceived $event): void
+    {
+        $payload = json_decode($event->message);
+        /**
+         * channel: `client-state` define on vue side
+         */
+        if ($payload->event != 'client-state') {
+            return;
+        }
+
+        $data = $payload->data;
+
+
+        Log::info(json_encode($data));
+    }
+}
